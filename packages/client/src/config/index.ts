fb12d0a3285aff609c3fc1a926874159b94ee93e
Ru Chern Chong <hello@ruchern.xyz>
2023-06-20 05:00:34 +0800

    Remove API URL since no longer used

--- packages/client/src/config/index.ts
+++ packages/client/src/config/index.ts
@@ -4,5 +4,4 @@ import timezone from "dayjs/plugin/timezone";
 dayjs.extend(timezone);
 dayjs.tz.setDefault("Asia/Singapore");
 
-export const DATA_GOV_SG_API_URL = `https://api.data.gov.sg/v1`;
 export const DEBOUNCE_WAIT_DURATION = 250;
