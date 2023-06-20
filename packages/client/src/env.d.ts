ae2da6d4249109fccee30adc5901985484f19575
Ru Chern Chong <hello@ruchern.xyz>
2023-06-21 01:24:13 +0800

    Remove env variables from types

--- packages/client/src/env.d.ts
+++ packages/client/src/env.d.ts
@@ -1,9 +1,6 @@
 /// <reference types="vite/client" />
 
-interface ImportMetaEnv {
-  readonly VITE_API_URL: string;
-  readonly VITE_GOOGLE_MAP_API_KEY: string;
-}
+interface ImportMetaEnv {}
 
 interface ImportMeta {
   readonly env: ImportMetaEnv;
